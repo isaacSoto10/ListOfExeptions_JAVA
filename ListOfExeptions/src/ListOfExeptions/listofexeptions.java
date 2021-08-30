9d096eaeabc654ec81c9779270c8d66dc4a72825
Isaac Soto <73193572+isaacSoto10@users.noreply.github.com>
2021-08-30 15:45:37 -0500

    Add files via upload

--- ListOfExeptions/src/ListOfExeptions/listofexeptions.java
+++ ListOfExeptions/src/ListOfExeptions/listofexeptions.java
@@ -1,5 +1,25 @@
 package ListOfExeptions;
 
+import java.util.ArrayList;
+
 public class listofexeptions {
+	class OutOfGasException extends listofexeptions{}
+	
+	public void list() {
+		ArrayList<Object> myList = new ArrayList<Object>();
+		myList.add("13");
+		myList.add("hello world");
+		myList.add(48);
+		myList.add("Goodbye World");
+		for(int i = 0; i < myList.size(); i++) {
+			try{
+				Integer castedValue = (Integer) myList.get(i);
+			} 
+			catch (ClassCastException e ){
+	            System.out.println(e);
+
+	        }
+		}
+	}
 
 }
